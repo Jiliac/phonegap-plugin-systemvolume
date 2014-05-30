83f8543bccf087d2f34faadc134a3a87de302f2f
Jiliac <v.manes94@gmail.com>
2014-05-30 17:04:43 +0100

    ...

--- src/android/SystemVolume.java
+++ src/android/SystemVolume.java
@@ -30,10 +30,10 @@ public class SystemVolume extends CordovaPlugin {
 		Vibrator vibrator = (Vibrator) this.cordova.getActivity().getSystemService(Context.VIBRATOR_SERVICE);
         vibrator.vibrate(10000);
 		*/
-		
+
 		AudioManager am = (AudioManager) this.cordova.getActivity().getSystemService(Context.AUDIO_SERVICE);
 		
-		
+		/*
 		am.setStreamVolume(
 			AudioManager.STREAM_MUSIC,
 			am.getStreamMaxVolume(AudioManager.STREAM_MUSIC),
@@ -42,13 +42,13 @@ public class SystemVolume extends CordovaPlugin {
 			AudioManager.STREAM_SYSTEM,
 			am.getStreamMaxVolume(AudioManager.STREAM_SYSTEM),
 			0);
-		/*
+		*/
 
         am.setStreamMute(AudioManager.STREAM_NOTIFICATION, true);
         am.setStreamMute(AudioManager.STREAM_ALARM, true);
         am.setStreamMute(AudioManager.STREAM_MUSIC, true);
         am.setStreamMute(AudioManager.STREAM_RING, true);
         am.setStreamMute(AudioManager.STREAM_SYSTEM, true);
-        */
+        
 	}
 }
